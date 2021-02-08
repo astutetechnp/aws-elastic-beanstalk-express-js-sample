7e838faaa0a02459bcff7074047a3cd190a2e2fe
Santosh Sah <sah.santosh@gmail.com>
2021-02-08 13:55:26 +0545

    Product API satrted

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+//home
 app.get("/", (req, res) => res.send("Hello World on AWS Cloud. Great!"));
+
+app.get("/products", (req, res) => res.send("products api "));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
